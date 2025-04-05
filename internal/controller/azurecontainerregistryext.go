7a721c43cbbb69d0dc5af84658774ed1f39c2f63
Aaron Spiegel <spiegela@gmail.com>
2025-04-05 18:20:17 -0500

    Add credential-set type

--- internal/controller/azurecontainerregistryext.go
+++ internal/controller/azurecontainerregistryext.go
@@ -21,7 +21,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/crossplane/provider-azurecontainerregistryext/internal/controller/config"
-	"github.com/crossplane/provider-azurecontainerregistryext/internal/controller/mytype"
+	"github.com/crossplane/provider-azurecontainerregistryext/internal/controller/credentialset"
 )
 
 // Setup creates all AzureContainerRegistryExt controllers with the supplied logger and adds them to
@@ -29,7 +29,7 @@ import (
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		config.Setup,
-		mytype.Setup,
+		credentialset.Setup,
 	} {
 		if err := setup(mgr, o); err != nil {
 			return err
